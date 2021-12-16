5625e15f9a14288b8d4082308c28d5c35a3c5b0f
edukng <eduk.kng@gmail.com>
2021-12-16 16:33:49 -0300

    Primeiro commit

---
+++ src/main/java/com/desenvolve/springtest/model/Student.java
@@ -0,0 +1,26 @@
+package com.desenvolve.springtest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String id;
+    private String name;
+    private String description;
+    private List<Course> courses;
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Student [id=%s, name=%s, description=%s, courses=%s]", id,
+                name, description, courses);
+    }
+}
